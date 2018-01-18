57aa709aa4d39d5fff848c18c9a4744d792bbdf2
DESKTOP - Carsten <carsten.dollack@yahoo.de>
2018-01-18 02:34:48 +0100

    [ADDED] Touch

--- Assets/Script/Modify.cs
+++ Assets/Script/Modify.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Modify : MonoBehaviour
 {
     Vector2 rot;
-	
-	// Update is called once per frame
-	void Update ()
+    public float speed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
     {
+#if UNITY_STANDALONE
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             RaycastHit hit;
@@ -18,13 +21,53 @@ public class Modify : MonoBehaviour
             }
 
         }
-            rot = new Vector2(rot.x + Input.GetAxis("Mouse X") * 3,
-                              rot.y + Input.GetAxis("Mouse Y") * 3);
+        rot = new Vector2(rot.x + Input.GetAxis("Mouse X") * 3,
+                          rot.y + Input.GetAxis("Mouse Y") * 3);
 
-            transform.localRotation = Quaternion.AngleAxis(rot.x, Vector3.up);
-            transform.localRotation *= Quaternion.AngleAxis(rot.y, Vector3.left);
+        transform.localRotation = Quaternion.AngleAxis(rot.x, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rot.y, Vector3.left);
 
-            transform.position += transform.forward * 3 * Input.GetAxis("Vertical");
-            transform.position += transform.right * 3 * Input.GetAxis("Horizontal");
-	}
+        transform.position += transform.forward * 3 * Input.GetAxis("Vertical");
+        transform.position += transform.right * 3 * Input.GetAxis("Horizontal");
+
+#endif
+
+
+#if UNITY_IPHONE
+
+
+        int TouchID = -1;
+
+        //Touch[]. myTouches = Input.touches;
+        
+        if (Input.touchCount > 0)
+	    {
+            foreach (Touch myTouch in Input.touches)
+            {
+                if (myTouch.phase == TouchPhase.Began)
+                {
+                    TouchID = myTouch.fingerId;
+                }
+
+                if (myTouch.phase != TouchPhase.Ended && myTouch.fingerId == TouchID)
+                {
+                    if (myTouch.position.x < Screen.width / 2) // Left side of screen
+                    {
+                        Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+                        transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+                    }
+
+                    if (myTouch.position.x > Screen.width / 2) // Right side of screen
+                    {
+
+                    }
+                }
+            }
+	    }
+
+
+
+#endif
+
+    }
 }
